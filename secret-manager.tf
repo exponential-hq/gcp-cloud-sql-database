304a8a47f65b12a036fe09bfd791cad56969b752
garry <garry.wilson@sequencehq.com>
2022-01-20 13:17:54 +0000

    Fix typo

--- secret-manager.tf
+++ secret-manager.tf
@@ -1,7 +1,7 @@
 resource "google_secret_manager_secret" "key" {
   count = var.store_credentials_in_secret_manager ? 1 : 0
 
-  secret_id = "database--${var.database_instance_namen}--${var.database_username}--password"
+  secret_id = "database--${var.database_instance_name}--${var.database_username}--password"
 
   replication {
     automatic = true
